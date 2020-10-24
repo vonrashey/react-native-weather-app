3ff6cb48dc89969e60cf8e20afc834a413717c39
Marashe Yeukai <vonrashey@gmail.com>
2020-10-24 11:34:45 +0200

    saved

--- App.js
+++ App.js
@@ -5,8 +5,9 @@ import * as Location from 'expo-location'
 import WeatherInfo from './components/WeatherInfo'
 import UnitsPicker from './components/UnitsPicker'
 import {colors} from './components/utils/index' 
-import {ReloadIcon} from './components/ReloadIcon'
-import { AppLoading } from 'expo';
+import ReloadIcon from './components/ReloadIcon'
+import WeatherDetails from './components/WeatherDetails'
+import { AppLoading } from 'expo'
 
 
 const WEATHER_API_KEY ='ddcb68b51e65dc098becc29be9333bce'
@@ -19,65 +20,67 @@ export default function App() {
   const [unitsSystem, setUnitsSystem] = useState('metric')
   useEffect(() => {
     load()
-  }, [unitsSystem])
+}, [unitsSystem])
 
-  async function load() {
+async function load() {
     setCurrentWeather(null)
     setErrorMessage(null)
-    try{
-      let {status} = await Location.requestPermissionsAsync()
-      if (status !== 'granted') {
-        setErrorMessage('Access to location is needed to run the app')
-        return
+    try {
+        let { status } = await Location.requestPermissionsAsync()
+
+        if (status !== 'granted') {
+            setErrorMessage('Access to location is needed to run the app')
+            return
+        }
+        const location = await Location.getCurrentPositionAsync()
+
+        const { latitude, longitude } = location.coords
+
+        const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
+
+        const response = await fetch(weatherUrl)
+
+        const result = await response.json()
+
+        if (response.ok) {
+            setCurrentWeather(result)
+        } else {
+            setErrorMessage(result.message)
+        }
+    } catch (error) {
+        setErrorMessage(error.message)
     }
-    const location = await Location.getCurrentPositionAsync()
-
-    const { latitude, longitude } = location.coords
-
-    const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
-   // alert(`Latitude : ${latitude}, Longitude : ${longitude}`)
-
-   const response = await fetch(weatherUrl)
-   const result = await response.json()
-
-   if(response.ok){
-     setCurrentWeather(result)
-   }else{
-     setErrorMessage(result.message)
-   }
-    }
-    catch(error) {
-      setErrorMessage(error.message)
-    }
-  }
-if(currentWeather){ 
-  return (
-    <View style={styles.container}>
-                <StatusBar style="auto" />
-                <View style={styles.main}>
+}
+if (currentWeather) {
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <View style={styles.main}>
                 <UnitsPicker unitsSystem={unitsSystem} setUnitsSystem={setUnitsSystem} />
-            <ReloadIcon load={load}/>
-            <WeatherInfo currentWeather={currentWeather}/>
-      </View>  
-    </View>
-  )
-}else if(errorMessage){
-  return (
-    <View style={styles.container}>
-      <ReloadIcon load={load}/>
-      <Text style={{ textAlign: 'center'}}>{errorMessage}</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
-} else{
-  return (
-    <View style={styles.container}>
-      <ActivityIndicator size='large' color={colors.PRIMARY_COLOR}/>
-      <StatusBar style="auto" />
-    </View>
-  )
+                <ReloadIcon load={load} />
+                <WeatherInfo currentWeather={currentWeather} />
+            </View>
+            <WeatherDetails currentWeather={currentWeather} unitsSystem={unitsSystem} />
+        </View>
+    )
+} else if (errorMessage) {
+    return (
+        <View style={styles.container}>
+            <ReloadIcon load={load} />
+            <Text style={{ textAlign: 'center' }}>{errorMessage}</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+} else {
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator size="large" color={colors.PRIMARY_COLOR} />
+            <StatusBar style="auto" />
+        </View>
+    )
 }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- components/ReloadIcon.js
+++ components/ReloadIcon.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { View, Platform, StyleSheet } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
-import { colors } from '../utils/index'
+import { colors } from './utils/index'
 
-export default function ReloadIcon({load}) {
-   const reloadIconName = Platform.OS ==='ios' ?'ios-refresh' : 'md-rfresh' 
-    return( 
+export default function ReloadIcon({ load }) {
+    const reloadIconName = Platform.OS === 'ios' ? 'ios-refresh' : 'md-refresh'
+    return (
         <View style={styles.reloadIcon}>
             <Ionicons onPress={load} name={reloadIconName} size={24} color={colors.PRIMARY_COLOR} />
         </View>
